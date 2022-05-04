c39a0961eb03ee0b19f42201b96b9256fdc3bfaa
Andreea Mincu <andreeamincu98@yahoo.com>
2022-05-04 13:02:13 +0300

    rename

--- app/src/main/java/com/example/bookreader/BookList/BookListAdapterGenres.java
+++ app/src/main/java/com/example/bookreader/BookList/BookListAdapterGenres.java
@@ -1,10 +1,13 @@
 package com.example.bookreader.BookList;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,43 +20,48 @@ import com.example.bookreader.AppPages.Document;
 import com.example.bookreader.Entities.Books;
 import com.example.bookreader.R;
 
+import java.util.ArrayList;
 import java.util.List;
-
-public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.BooksHolder>{
+public class BookListAdapterGenres extends RecyclerView.Adapter<BookListAdapterSearch.BooksHolder>{
 
     List<Books> list;
     Activity activity;
 
-    public BookListAdapter(List<Books> list, Activity activity) {
+    public BookListAdapterGenres(List<Books> list, Activity activity) {
         this.list = list;
         this.activity = activity;
     }
 
     @NonNull
     @Override
-    public BooksHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new BooksHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_books,parent,false));
+    public BookListAdapterSearch.BooksHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new BookListAdapterSearch.BooksHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_books,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BooksHolder holder, int position) {
-        Books books= list.get(position);
+    public void onBindViewHolder(@NonNull BookListAdapterSearch.BooksHolder holder, int position) {
+        Books books = list.get(position);
 
         holder.title.setText(books.getTitle());
         Glide.with(activity).load(books.getCover()).transition(DrawableTransitionOptions.withCrossFade()).into(holder.cover);
         holder.itemView.setOnClickListener(v -> {
-            Intent intent=new Intent(activity, Document.class);
-            intent.putExtra("title",books.getTitle());
-            intent.putExtra("url",books.getUrl());
-            intent.putExtra("genre",books.getGenre());
+            Intent intent = new Intent(activity, Document.class);
+            intent.putExtra("title", books.getTitle());
+            intent.putExtra("url", books.getUrl());
+            intent.putExtra("genre", books.getGenre());
             activity.startActivity(intent);
         });
+
     }
 
     @Override
     public int getItemCount() {
+
         return list.size();
+
     }
+
+
 
     public static class BooksHolder extends RecyclerView.ViewHolder{
 
